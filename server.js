c422fc04e92326d8837de522a88ca5c9eb908314
Abdallah Day <10871403+AbdallahDay@users.noreply.github.com>
2018-10-14 00:15:05 -0500

    attempting to fix heroku error

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers/burgers_controller');
 
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
-const app = express();
+var app = express();
 
 app.use(express.static("public"));
 
